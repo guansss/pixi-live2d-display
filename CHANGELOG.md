3b711b8a80c9f1e6a09c05c9cdc02749ec1fa22f
Jun Koyama <koyama@live2d.com>
2020-01-30 18:28:13 +0900

    Update to Cubism 4 SDK for Web R1

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
+
+
+## [4-r.1] - 2020-01-30
+
+### Added
+
+* Add `.editorconfig`, `.gitattributes` and `.gitignore`.
+* Add document `README.md` and `CHANGELOG.md`.
+* Add `package.json` for development and build.
+* Add Prettier and ESLint for format and check code quolity.
+
+### Changed
+
+* Move source files to `/src` directory.
+* Reformat code using Prettier and ESLint.
+
+
+[4-r.1]: https://github.com/Live2D/CubismWebFramework/compare/ce2585a919ac6e99f64dd468933772c6f1abbcc7...4-r.1
---
+++ src/cubismdefaultparameterid.ts
@@ -0,0 +1,65 @@
+/**
+ * Copyright(c) Live2D Inc. All rights reserved.
+ *
+ * Use of this source code is governed by the Live2D Open Software license
+ * that can be found at https://www.live2d.com/eula/live2d-open-software-license-agreement_en.html.
+ */
+
+/**
+ * @brief パラメータIDのデフォルト値を保持する定数<br>
+ *         デフォルト値の仕様は以下のマニュアルに基づく<br>
+ *         https://docs.live2d.com/cubism-editor-manual/standard-parametor-list/
+ */
+export namespace Live2DCubismFramework {
+  // パーツID
+  export const HitAreaPrefix = 'HitArea';
+  export const HitAreaHead = 'Head';
+  export const HitAreaBody = 'Body';
+  export const PartsIdCore = 'Parts01Core';
+  export const PartsArmPrefix = 'Parts01Arm_';
+  export const PartsArmLPrefix = 'Parts01ArmL_';
+  export const PartsArmRPrefix = 'Parts01ArmR_';
+
+  // パラメータID
+  export const ParamAngleX = 'ParamAngleX';
+  export const ParamAngleY = 'ParamAngleY';
+  export const ParamAngleZ = 'ParamAngleZ';
+  export const ParamEyeLOpen = 'ParamEyeLOpen';
+  export const ParamEyeLSmile = 'ParamEyeLSmile';
+  export const ParamEyeROpen = 'ParamEyeROpen';
+  export const ParamEyeRSmile = 'ParamEyeRSmile';
+  export const ParamEyeBallX = 'ParamEyeBallX';
+  export const ParamEyeBallY = 'ParamEyeBallY';
+  export const ParamEyeBallForm = 'ParamEyeBallForm';
+  export const ParamBrowLY = 'ParamBrowLY';
+  export const ParamBrowRY = 'ParamBrowRY';
+  export const ParamBrowLX = 'ParamBrowLX';
+  export const ParamBrowRX = 'ParamBrowRX';
+  export const ParamBrowLAngle = 'ParamBrowLAngle';
+  export const ParamBrowRAngle = 'ParamBrowRAngle';
+  export const ParamBrowLForm = 'ParamBrowLForm';
+  export const ParamBrowRForm = 'ParamBrowRForm';
+  export const ParamMouthForm = 'ParamMouthForm';
+  export const ParamMouthOpenY = 'ParamMouthOpenY';
+  export const ParamCheek = 'ParamCheek';
+  export const ParamBodyAngleX = 'ParamBodyAngleX';
+  export const ParamBodyAngleY = 'ParamBodyAngleY';
+  export const ParamBodyAngleZ = 'ParamBodyAngleZ';
+  export const ParamBreath = 'ParamBreath';
+  export const ParamArmLA = 'ParamArmLA';
+  export const ParamArmRA = 'ParamArmRA';
+  export const ParamArmLB = 'ParamArmLB';
+  export const ParamArmRB = 'ParamArmRB';
+  export const ParamHandL = 'ParamHandL';
+  export const ParamHandR = 'ParamHandR';
+  export const ParamHairFront = 'ParamHairFront';
+  export const ParamHairSide = 'ParamHairSide';
+  export const ParamHairBack = 'ParamHairBack';
+  export const ParamHairFluffy = 'ParamHairFluffy';
+  export const ParamShoulderY = 'ParamShoulderY';
+  export const ParamBustX = 'ParamBustX';
+  export const ParamBustY = 'ParamBustY';
+  export const ParamBaseX = 'ParamBaseX';
+  export const ParamBaseY = 'ParamBaseY';
+  export const ParamNONE = 'NONE:';
+}
