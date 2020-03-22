bd7a7f6fab63f3dccd91c554357d221bae6db64b
guansss <821143943@qq.com>
2020-03-22 20:40:58 +0800

    Add missing utils file

---
+++ src/utils/math.ts
@@ -0,0 +1,11 @@
+/**
+ * These functions can be slightly faster than the ones in Lodash.
+ */
+
+export function clamp(num: number, lower: number, upper: number) {
+    return num < lower ? lower : num > upper ? upper : num;
+}
+
+export function rand(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+}
