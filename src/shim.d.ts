18c979d0663afd51d78f58c3972687fefb1a2afc
guansss <821143943@qq.com>
2020-03-23 23:46:24 +0800

    Add size test for Live2DModel

--- src/shim.d.ts
+++ src/shim.d.ts
@@ -1,61 +1,34 @@
 /**
  * PIXI shims
  */
-declare module '@pixi/constants' {
-    export { BLEND_MODES, DRAW_MODES } from 'pixi.js';
-}
 
 declare module '@pixi/core' {
-    import { State } from 'pixi.js';
-
-    class ExposedState extends State {
-        static for2d(): State; // this is not defined in pixi's types
-    }
-
-    export { ExposedState as State };
-    export {
-        Buffer,
-        Geometry,
-        Renderer,
-        AbstractBatchRenderer as BatchRenderer, // use a name trick because BatchRenderer is not defined in pixi's types
-        Shader,
-        Texture,
-        BaseTexture,
-    } from 'pixi.js';
+    export { Renderer, Shader, Texture, BaseTexture } from 'pixi.js';
 }
 
 declare module '@pixi/loaders' {
-    export { Loader,LoaderResource } from 'pixi.js';
+    export { Loader, LoaderResource } from 'pixi.js';
 }
 
 declare module '@pixi/utils' {
     import { utils } from 'pixi.js';
     export import EventEmitter = utils.EventEmitter;
-    export import sayHello = utils.sayHello;
 }
 
 declare module '@pixi/app' {
     export { Application } from 'pixi.js';
 }
 
+declare module '@pixi/ticker' {
+    export { TickerPlugin } from 'pixi.js';
+}
+
 declare module '@pixi/display' {
     export { DisplayObject, Container } from 'pixi.js';
 }
 
-declare module '@pixi/particles' {
-    export { ParticleContainer, ParticleRenderer } from 'pixi.js';
-}
-
-declare module '@pixi/spritesheet' {
-    export { SpritesheetLoader } from 'pixi.js';
-}
-
 declare module '@pixi/sprite' {
     export { Sprite } from 'pixi.js';
-}
-
-declare module '@pixi/mesh' {
-    export { Mesh } from 'pixi.js';
 }
 
 declare module '@pixi/math' {
--- test/Live2DModel.test.js
+++ test/Live2DModel.test.js
@@ -1,7 +1,45 @@
 import { Live2DModel } from '../src';
+import { TickerPlugin } from '@pixi/ticker';
+import { Application } from '@pixi/app';
+import assert from 'assert';
+import { LOGICAL_HEIGHT, LOGICAL_WIDTH } from '../src/live2d/Live2DInternalModel';
 
-describe('Typescript usage suite', () => {
-    it('should be able to execute a test', () => {
-        const m = Live2DModel.fromModelSettingsFile('test/assets/shizuku/shizuku.model.json');
+Application.registerPlugin(TickerPlugin);
+
+const app = new Application({
+    autoStart: true,
+    width: 100,
+    height: 100,
+});
+
+describe('Live2DModel', async () => {
+    /** @type {Live2DModel} */
+    let model;
+
+    before(async () => {
+        model = await Live2DModel.fromModelSettingsFile('test/assets/shizuku/shizuku.model.json');
+        app.stage.addChild(model);
+    });
+
+    after(() => {
+        model.destroy();
+    });
+
+    it('should have correct size', () => {
+        assert.equal(model.internal.originalWidth, 1280);
+        assert.equal(model.internal.originalHeight, 1380);
+
+        const modelWidth = model.internal.originalWidth * (model.internal.modelSettings.layout.width || LOGICAL_WIDTH) / LOGICAL_WIDTH;
+        const modelHeight = model.internal.originalHeight * (model.internal.modelSettings.layout.height || LOGICAL_HEIGHT) / LOGICAL_HEIGHT;
+
+        assert.equal(model.width, modelWidth);
+        assert.equal(model.height, modelHeight);
+
+        model.scale.set(10, 0.1);
+
+        assert.equal(model.width, modelWidth * 10);
+        assert.equal(model.height, modelHeight * 0.1);
+
+        model.scale.set(1, 1);
     });
 });
