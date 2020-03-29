66d52a4c9ff4a2496ad5a25fd046762c4bc0f4dd
guansss <821143943@qq.com>
2020-03-30 05:02:35 +0800

    Add motion binding in browser test

--- test/browser/BrowserTest.js
+++ test/browser/BrowserTest.js
@@ -14,6 +14,8 @@
     async function loadModel() {
         model = await PIXI.Live2D.Live2DModel.fromModelSettingsFile(TEST_MODEL.file);
         app.stage.addChild(model);
+
+        model.on('hit', hitAreas => hitAreas.includes('body') && model.internal.motionManager.startRandomMotion('tapBody'));
     }
 
     window.BrowserTest = {
