9f9278bd4680f60179d3a373fd72adf111afea7c
guansss <821143943@qq.com>
2020-12-15 23:43:35 +0800

    Fix missing export

--- src/cubism4/index.ts
+++ src/cubism4/index.ts
@@ -2,5 +2,4 @@ export * from '@/cubism4/Cubism4ExpressionManager';
 export * from '@/cubism4/Cubism4ModelSettings';
 export * from '@/cubism4/Cubism4MotionManager';
 export * from '@/cubism4/Cubism4InternalModel';
-
-import '@/cubism4/setup';
+export * from '@/cubism4/setup';
