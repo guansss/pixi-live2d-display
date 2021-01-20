d7a73a88413d4a96e9600a602112735e5bdf7cb0
guansss <821143943@qq.com>
2021-01-21 00:06:04 +0800

    Use the local Pixi bundle in browser test

--- test/browser/index.js
+++ test/browser/index.js
@@ -3,7 +3,7 @@ import { TEST_MODEL } from '../env';
 import { createApp, loadScript } from '../utils';
 
 describe('Browser <script> tag', function() {
-    this.timeout(2000);
+    this.timeout(1000);
 
     it('should work with Pixi in browser', async function() {
         // provide globals for browser test, don't directly assign them to window
@@ -14,14 +14,18 @@ describe('Browser <script> tag', function() {
             window[key] = env[key];
         }
 
-        await loadScript('https://cdn.jsdelivr.net/npm/pixi.js@5.3.7/dist/pixi.min.js');
+        await loadScript('../node_modules/pixi.js/dist/pixi.min.js');
 
         // prevent Pixi from flagging WebGL as unsupported in headless test
         // https://github.com/pixijs/pixi.js/issues/6109
         PIXI.settings.FAIL_IF_MAJOR_PERFORMANCE_CAVEAT = false;
 
-        // a production build is required!!
-        await loadScript('../dist/index.js');
+        try {
+            // a production build is required!!
+            await loadScript('../dist/index.js');
+        } catch (e) {
+            console.warn('Failed to load "dist/index.js". Have you run `yarn build`?');
+        }
 
         // load original, non-bundled source
         await loadScript('./browser/BrowserTest.js');
