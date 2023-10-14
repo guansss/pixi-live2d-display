16dd4a3e1b8375a4ea405c877eec4fbc54d4c0d5
Guan <821143943@qq.com>
2023-10-15 01:22:38 +0800

    test: load cubism cores

---
+++ test/setup.ts
@@ -0,0 +1,12 @@
+import "./rpc/image-snapshot-client";
+
+declare const __CUBISM2_CORE_SOURCE__: string;
+declare const __CUBISM4_CORE_SOURCE__: string;
+
+(() => {
+    __CUBISM2_CORE_SOURCE__;
+})();
+(() => {
+    __CUBISM4_CORE_SOURCE__;
+    window.Live2DCubismCore = Live2DCubismCore;
+})();
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 /// <reference types="vitest" />
 
 import path from "path";
-import { defineConfig } from "vite";
+import { UserConfig, defineConfig } from "vite";
 import packageJson from "./package.json";
 import { testRpcPlugin } from "./test/rpc/rpc-server";
+import { existsSync, readFileSync } from "fs";
+
+const cubism2Core = path.resolve(__dirname, "core/live2d.min.js");
+const cubism4Core = path.resolve(__dirname, "core/live2dcubismcore.js");
+
+if (!existsSync(cubism2Core) || !existsSync(cubism4Core)) {
+    throw new Error("Cubism Core not found. Please run `npm run setup` to download them.");
+}
 
 export default defineConfig(({ command, mode }) => {
     const isDev = command === "serve";
@@ -47,14 +55,35 @@ export default defineConfig(({ command, mode }) => {
             },
             minify: false,
         },
-        plugins: [testRpcPlugin()],
+        plugins: [
+            testRpcPlugin(),
+            {
+                name: "load-cubism-core",
+                enforce: "post",
+                transform(code, id) {
+                    if (id.includes("test/setup.ts")) {
+                        code = code.replace(
+                            "__CUBISM2_CORE_SOURCE__",
+                            readFileSync(cubism2Core, "utf-8"),
+                        );
+                        code = code.replace(
+                            "__CUBISM4_CORE_SOURCE__",
+                            readFileSync(cubism4Core, "utf-8"),
+                        );
+
+                        return { code };
+                    }
+                },
+            },
+        ],
         test: {
             include: ["**/*.test.ts"],
             browser: {
                 enabled: true,
                 name: "chrome",
+                slowHijackESM: false,
             },
-            setupFiles: ["./test/rpc/image-snapshot-client.ts"],
+            setupFiles: ["./test/setup.ts"],
         },
     };
 });
